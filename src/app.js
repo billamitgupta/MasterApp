2110592f2b34116ebce93f725760c54c61845074
Amit gupta <billamitgupta@gmail.com>
2024-03-20 22:07:06 +0530

    config completed

--- src/app.js
+++ src/app.js
@@ -12,5 +12,12 @@ app.use(express.urlencoded({extended: true , limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//routes import
+import userRouter from './routes/user.routes.js'
+
+//routes decleartion
+app.use("/api/v1/users", userRouter)
+
+//http://localhost:9000/api/v1/users/register
 
 export {app}
